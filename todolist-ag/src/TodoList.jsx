480c3f9043dda94e486f485b30ad33bf0dcf9a9f
Tuukkaani <bgh463@myy.haaga-helia.fi>
2024-10-07 05:25:27 +0300

    Added MUI Date-Picker.

--- todolist-ag/src/TodoList.jsx
+++ todolist-ag/src/TodoList.jsx
@@ -1,25 +1,43 @@
-import { useState } from "react";
+import { useState, useRef, useMemo } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
 
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
 function TodoList() {
     const [todo, setTodo] = useState({
         description: "",
-        date: "",
+        date: null,
         priority: ""
     });
 
     const [todos, setTodos] = useState([]);
 
+    const gridRef = useRef();
+
+    const rowSelection = useMemo(() => { 
+        return {
+            mode: 'singleRow',
+        };
+    }, []);
+
     const columnDefs = [
-        { field: "description",filter: "agTextColumnFilter", floatingFilter: true },
-        { field: "date", filter: "agTextColumnFilter", floatingFilter: true },
-        { field: "priority", filter: "agTextColumnFilter", floatingFilter: true }
+        { field: "description", filter: "agTextColumnFilter", floatingFilter: true },
+        { field: "date", filter: "agTextColumnFilter", floatingFilter: true,},
+        { field: "priority", filter: "agTextColumnFilter", floatingFilter: true, 
+            cellStyle: params => params.value === "High" ? { color: 'red' } : { color: 'black' }
+        }
     ];
 
     const handleInput = (event) => {
         setTodo({ ...todo, [event.target.name]: event.target.value });
+    };
+
+    const handleDate = (newDate) => {
+        setTodo({...todo, date: newDate});
     };
 
     const addTodo = () => {
@@ -30,43 +48,56 @@ function TodoList() {
 
         setTodos([...todos, todo]);
         setTodo({
-            date: "",
+            date: null,
             description: "",
             priority: ""
         });
     };
 
+    const handleDelete = () => {
+        if (gridRef.current.getSelectedNodes().length > 0) {
+            setTodos(todos.filter((todo, index) =>
+                index != gridRef.current.getSelectedNodes()[0].id))
+        }
+        else {
+            alert('Select a row first!');
+        }
+    };
+
     return (
         <>
             <div>
-                <input 
-                    type="text" 
-                    onChange={handleInput} 
-                    placeholder="Description" 
-                    name="description" 
-                    value={todo.description} 
+                <input
+                    type="text"
+                    onChange={handleInput}
+                    placeholder="Description"
+                    name="description"
+                    value={todo.description}
                 />
-                <input 
-                    type="text" 
-                    onChange={handleInput} 
-                    placeholder="Date" 
-                    name="date" 
-                    value={todo.date} 
-                />
-
-                <input 
-                    type="text" 
-                    onChange={handleInput} 
-                    placeholder="Priority" 
-                    name="priority" 
-                    value={todo.priority} 
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker 
+                        label="Date"
+                        value={todo.date}
+                        onChange={handleDate}
+                    />
+                </LocalizationProvider>
+                <input
+                    type="text"
+                    onChange={handleInput}
+                    placeholder="Priority"
+                    name="priority"
+                    value={todo.priority}
                 />
                 <button onClick={addTodo}>Add</button>
+                <button onClick={handleDelete}>Delete</button>
             </div>
             <div className="ag-theme-material" style={{ width: 700, height: 500 }}>
                 <AgGridReact
+                    ref={gridRef}
+                    onGridReady={params => gridRef.current = params.api}
                     rowData={todos}
                     columnDefs={columnDefs}
+                    rowSelection={rowSelection}
                     defaultColDef={{
                         floatingFilter: true,
                         filter: true,
