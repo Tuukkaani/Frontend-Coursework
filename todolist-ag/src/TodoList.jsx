3753e9ff17952c9a620035e9e20c329b2200dc4d
Tuukkaani <bgh463@myy.haaga-helia.fi>
2024-10-07 05:37:53 +0300

    Added MUI Tabs.

--- todolist-ag/src/TodoList.jsx
+++ todolist-ag/src/TodoList.jsx
@@ -7,6 +7,12 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+
 function TodoList() {
     const [todo, setTodo] = useState({
         description: "",
@@ -18,7 +24,7 @@ function TodoList() {
 
     const gridRef = useRef();
 
-    const rowSelection = useMemo(() => { 
+    const rowSelection = useMemo(() => {
         return {
             mode: 'singleRow',
         };
@@ -26,18 +32,25 @@ function TodoList() {
 
     const columnDefs = [
         { field: "description", filter: "agTextColumnFilter", floatingFilter: true },
-        { field: "date", filter: "agTextColumnFilter", floatingFilter: true,},
-        { field: "priority", filter: "agTextColumnFilter", floatingFilter: true, 
+        { field: "date", filter: "agTextColumnFilter", floatingFilter: true, },
+        {
+            field: "priority", filter: "agTextColumnFilter", floatingFilter: true,
             cellStyle: params => params.value === "High" ? { color: 'red' } : { color: 'black' }
         }
     ];
+
+    const [tabValue, setTabValue] = useState("1");
 
     const handleInput = (event) => {
         setTodo({ ...todo, [event.target.name]: event.target.value });
     };
 
     const handleDate = (newDate) => {
-        setTodo({...todo, date: newDate});
+        setTodo({ ...todo, date: newDate });
+    };
+
+    const handleTabs = (event, newTab) => {
+        setTabValue(newTab);
     };
 
     const addTodo = () => {
@@ -66,44 +79,58 @@ function TodoList() {
 
     return (
         <>
-            <div>
-                <input
-                    type="text"
-                    onChange={handleInput}
-                    placeholder="Description"
-                    name="description"
-                    value={todo.description}
-                />
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker 
-                        label="Date"
-                        value={todo.date}
-                        onChange={handleDate}
-                    />
-                </LocalizationProvider>
-                <input
-                    type="text"
-                    onChange={handleInput}
-                    placeholder="Priority"
-                    name="priority"
-                    value={todo.priority}
-                />
-                <button onClick={addTodo}>Add</button>
-                <button onClick={handleDelete}>Delete</button>
-            </div>
-            <div className="ag-theme-material" style={{ width: 700, height: 500 }}>
-                <AgGridReact
-                    ref={gridRef}
-                    onGridReady={params => gridRef.current = params.api}
-                    rowData={todos}
-                    columnDefs={columnDefs}
-                    rowSelection={rowSelection}
-                    defaultColDef={{
-                        floatingFilter: true,
-                        filter: true,
-                    }}
-                />
-            </div>
+            <Box sx={{ width: '100%', typography: 'body1' }}>
+                <TabContext value={tabValue}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <TabList onChange={handleTabs} aria-label="lab API tabs example">
+                            <Tab label="Home" value="1" />
+                            <Tab label="ToDoList" value="2" />
+                        </TabList>
+                    </Box>
+                    <TabPanel value="1">HELLO TEAM! WELCOME TO TODO!</TabPanel>
+                    <TabPanel value="2">
+                        <div>
+                            <input
+                                type="text"
+                                onChange={handleInput}
+                                placeholder="Description"
+                                name="description"
+                                value={todo.description}
+                            />
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DatePicker
+                                    label="Date"
+                                    value={todo.date}
+                                    onChange={handleDate}
+                                />
+                            </LocalizationProvider>
+                            <input
+                                type="text"
+                                onChange={handleInput}
+                                placeholder="Priority"
+                                name="priority"
+                                value={todo.priority}
+                            />
+                            <button onClick={addTodo}>Add</button>
+                            <button onClick={handleDelete}>Delete</button>
+                        </div>
+                        <div className="ag-theme-material" style={{ width: 700, height: 500 }}>
+                            <AgGridReact
+                                ref={gridRef}
+                                onGridReady={params => gridRef.current = params.api}
+                                rowData={todos}
+                                columnDefs={columnDefs}
+                                rowSelection={rowSelection}
+                                defaultColDef={{
+                                    floatingFilter: true,
+                                    filter: true,
+                                }}
+                            />
+                        </div>
+                    </TabPanel>
+                </TabContext>
+            </Box>
+
         </>
     );
 }
